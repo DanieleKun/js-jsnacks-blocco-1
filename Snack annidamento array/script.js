f9815a886b1852d82a9e3e9c5e84d7c26d283f50
Daniele <pucciodaniele95@gmail.com>
2022-05-10 17:05:30 +0200

    finish EX

--- Snack annidamento array/script.js
+++ Snack annidamento array/script.js
@@ -1,27 +1,24 @@
 // #crea 2 array, di 5 stringhe il primo e di 2 stringhe il secondo, stampa dentro una UL dove ogni LI ha come
-//  contenuto principale in ordine, le parole del primo array e come sotto-contenuto annidato le 2 parole del secondo array in una UL annidata, tipo:
-// <ul>
-//  	<li>parola1 arr1
-// 		<ul>
-//  			<li>parola1 arr2</li>
-// 			<li>parola2 arr2</li>
-// 		</ul>
-// 	</li>
-// 	<li>parola2 arr1
-// 		<ul>
-//  			<li>parola1 arr2</li>
-// 			<li>parola2 arr2</li>
-// 		</ul>
-// 	</li>
-// 	<li>parola3 arr1
-// 	…etc
+//  contenuto principale in ordine, le parole del primo array e come sotto-contenuto annidato le 2 parole del secondo array in una UL annidata.
 
 const myArray1 = ["Parola 1", "Parola 2", "Parola 3", "Parola 4", "Parola 5"];
 const myArray2 = ["parola 1", "parola 2"];
-const myList = document.querySelector(".list")
+const myList = document.querySelector(".list");
 // console.log(myArray1, myArray2);
 
 for (let i = 0; i < myArray1.length; i++) {
-    myList.innerHTML += `<li>${myArray1[i]}</li>`;
-    console.log(myList);
+
+    let myFirstLi = document.createElement('li');
+    myFirstLi.append(myArray1[i]);
+    myList.append(myFirstLi);
+    // console.log(myList);
+
+    let myUl = document.createElement("ul");
+    let myLi = document.createElement("li");
+
+    for (let j = 0; j < myArray2.length; j++) {
+        myLi.append(myArray2[j]);
+        myUl.append(myLi);
+        myFirstLi.append(myUl);
+    }
 }
